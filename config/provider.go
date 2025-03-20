f98f94992256d213ac587af84420eb674805b879
mihaifat <fatmihaialexandru@gmail.com>
2025-03-20 15:50:06 +0200

    Changed root group to "auth0.jwefers.upbound.io"

--- config/provider.go
+++ config/provider.go
@@ -83,7 +83,7 @@ var providerMetadata string
 // GetProvider returns provider configuration
 func GetProvider() *ujconfig.Provider {
 	pc := ujconfig.NewProvider([]byte(providerSchema), resourcePrefix, modulePath, []byte(providerMetadata),
-		ujconfig.WithRootGroup("auth0.com"),
+		ujconfig.WithRootGroup("auth0.jwefers.upbound.io"),
 		ujconfig.WithIncludeList(ExternalNameConfigured()),
 		ujconfig.WithFeaturesPackage("internal/features"),
 		ujconfig.WithDefaultResourceOptions(
